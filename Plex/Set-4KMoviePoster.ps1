9adff4249721c3675b40326ad5e180f989b8f5a1
Jonathan Moss <jwmoss88@gmail.com>
2020-08-24 22:27:35 -0400

    Remove should process

--- Plex/Set-4KMoviePoster.ps1
+++ Plex/Set-4KMoviePoster.ps1
@@ -1,5 +1,5 @@
 function Set-4KMoviePoster {
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    [CmdletBinding()]
     param (
         [string]
         $PlexToken,
@@ -46,50 +46,48 @@ function Set-4KMoviePoster {
             $4K_PlexLibrary_results = Invoke-RestMethod -uri $4K_PlexLibrary_url
         }
 
-        if ($PSCmdlet.ShouldProcess("ShouldProcess?")) {
-            foreach ($m in $4K_PlexLibrary_results) {
-                ## get the IMDB Id by extracing the numbers from the GUID of the movie result
-                $imdbid_plex = "tt" + ($m.guid -replace "[^0-9]" , '')
+        foreach ($m in $4K_PlexLibrary_results) {
+            ## get the IMDB Id by extracing the numbers from the GUID of the movie result
+            $imdbid_plex = "tt" + ($m.guid -replace "[^0-9]" , '')
                 
-                ## Get the movie from TMDB
-                $tmdb = Get-TMDBMovie -ImdbID $imdbid_plex -API $TMDBToken
+            ## Get the movie from TMDB
+            $tmdb = Get-TMDBMovie -ImdbID $imdbid_plex -API $TMDBToken
 
-                ## Create the folder where we'll store the posters
-                $null = New-Item -Name "PlexMoviePosters" -Path (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters") -Force
+            ## Create the folder where we'll store the posters
+            $null = New-Item -Name "PlexMoviePosters" -Path (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters") -Force
 
-                if (-not (Test-Path (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id)))) {
+            if (-not (Test-Path (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id)))) {
                     
-                    Write-Verbose ("Creating folder - ($(Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id)))")
+                Write-Verbose ("Creating folder - ($(Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id)))")
 
-                    ## create the folder for the movie poster
-                    $null = New-Item -Path (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters") -Name $tmdb.Id -ItemType Directory
+                ## create the folder for the movie poster
+                $null = New-Item -Path (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters") -Name $tmdb.Id -ItemType Directory
                     
-                    Write-Verbose "Downloading the $($m.title)"
-                    ## Picture path 
-                    $picture = (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id) "poster.jpg")
+                Write-Verbose "Downloading the $($m.title)"
+                ## Picture path 
+                $picture = (Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id) "poster.jpg")
                     
-                    ## Download the poster
-                    Invoke-WebRequest -Uri "https://image.tmdb.org/t/p/original$($tmdb.poster_path)" -OutFile $picture
+                ## Download the poster
+                Invoke-WebRequest -Uri "https://image.tmdb.org/t/p/original$($tmdb.poster_path)" -OutFile $picture
 
-                    Write-Verbose "Processing $($m.title) - $($tmdb.Id) with magick" 
-                    ## set the 4K picture path
-                    $4k_poster = Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id) "4kposter.jpg"
-                    
-                    ## using magick, apply the 4K banner to the downloaded poster
-                    magick $picture $4KLogoPath -resize x"%[fx:t?u.h*0.1:u.h]" -background black -gravity north -extent "%[fx:u.w]x%[fx:s.h]" -composite $4k_poster
+                Write-Verbose "Processing $($m.title) - $($tmdb.Id) with magick" 
+                ## set the 4K picture path
+                $4k_poster = Join-Path ([IO.Path]::GetTempPath()) "PlexMoviePosters" $($tmdb.Id) "4kposter.jpg"
+
+                ## using magick, apply the 4K banner to the downloaded poster
+                magick $picture $4KLogoPath -resize x"%[fx:t?u.h*0.1:u.h]" -background black -gravity north -extent "%[fx:u.w]x%[fx:s.h]" -composite $4k_poster
                 
-                    $metadata_4k_url = $PlexURL + "/library/metadata/$($m.ratingkey)/posters?includeExternalMedia=1&X-Plex-Token=$plextoken"
+                $metadata_4k_url = $PlexURL + "/library/metadata/$($m.ratingkey)/posters?includeExternalMedia=1&X-Plex-Token=$plextoken"
 
-                    $plexparams = @{
-                        URI    = $metadata_4k_url
-                        Method = "POST"
-                        InFile = $4k_poster
-                    }
-                    Write-Verbose "Setting 4K poster for $($m.Title)"
-
-                    Invoke-RestMethod @plexparams
-            
+                $plexparams = @{
+                    URI    = $metadata_4k_url
+                    Method = "POST"
+                    InFile = $4k_poster
                 }
+                Write-Verbose "Setting 4K poster for $($m.Title)"
+
+                Invoke-RestMethod @plexparams
+            
             }
         }
 
